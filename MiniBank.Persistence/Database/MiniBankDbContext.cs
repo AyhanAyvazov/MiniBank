8d8941960ab98c0a1499d0197ba4da951ec6bce9
aikhan <ayhanayvazov00@gmail.com>
2025-01-25 00:17:56 +0400

    Added Seed Data

--- MiniBank.Persistence/Database/MiniBankDbContext.cs
+++ MiniBank.Persistence/Database/MiniBankDbContext.cs
@@ -5,11 +5,6 @@ using MiniBank.Domain.Entities.Customers;
 using MiniBank.Domain.Entities.Roles;
 using MiniBank.Domain.Entities.Transactions;
 using MiniBank.Domain.Entities.Users;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniBank.Persistence.Database
 {
@@ -32,6 +27,35 @@ namespace MiniBank.Persistence.Database
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AccountType>().HasData(
+      new AccountType { Id = 1, Name = "Savings", Description = "Savings account" },
+      new AccountType { Id = 2, Name = "Current", Description = "Current account" }
+  );
+
+            modelBuilder.Entity<Currency>().HasData(
+        new Currency { Id = 1, Code = "USD", Name = "US Dollar", Symbol = "$" },
+        new Currency { Id = 2, Code = "EUR", Name = "Euro", Symbol = "€" },
+        new Currency { Id = 3, Code = "GEL", Name = "Georgian Lari", Symbol = "₾" },
+        new Currency { Id = 4, Code = "TL", Name = "Turkish Lira", Symbol = "₺" }
+    );
+            modelBuilder.Entity<Role>().HasData(
+        new Role { Id = 1, Name = "Admin" },
+        new Role { Id = 2, Name = "Employer" },
+        new Role { Id = 3, Name = "Customer" }
+
+    );
+            modelBuilder.Entity<TransactionStatus>().HasData(
+      new TransactionStatus { Id = 1, Name = "Pending" },
+      new TransactionStatus { Id = 2, Name = "Completed" },
+      new TransactionStatus { Id = 3, Name = "Failed" },
+      new TransactionStatus { Id = 4, Name = "Cancelled" }
+  );
+            modelBuilder.Entity<TransactionType>().HasData(
+      new TransactionType { Id = 1, Name = "Deposit" },
+      new TransactionType { Id = 2, Name = "Withdrawal" },
+      new TransactionType { Id = 3, Name = "Transfer" }
+  );
         }
 
     }
---
+++ MiniBank.Persistence/Migrations/20250124201034_AddedSeedData.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MiniBank.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AccountTypes",
+                columns: new[] { "Id", "CreatedDate", "Description", "IsDeleted", "Name", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(28), "Savings account", false, "Savings", null },
+                    { 2, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(43), "Current account", false, "Current", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "Id", "Code", "CreatedDate", "IsDeleted", "Name", "Symbol", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, "USD", new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(227), false, "US Dollar", "$", null },
+                    { 2, "EUR", new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(229), false, "Euro", "€", null },
+                    { 3, "GEL", new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(231), false, "Georgian Lari", "₾", null },
+                    { 4, "TL", new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(233), false, "Turkish Lira", "₺", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "CreatedDate", "IsDeleted", "Name", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(264), false, "Admin", null },
+                    { 2, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(266), false, "Employer", null },
+                    { 3, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(267), false, "Customer", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionStatuses",
+                columns: new[] { "Id", "CreatedDate", "IsDeleted", "Name", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(305), false, "Pending", null },
+                    { 2, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(307), false, "Completed", null },
+                    { 3, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(308), false, "Failed", null },
+                    { 4, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(309), false, "Cancelled", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "Id", "CreatedDate", "IsDeleted", "Name", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(346), false, "Deposit", null },
+                    { 2, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(348), false, "Withdrawal", null },
+                    { 3, new DateTime(2025, 1, 25, 0, 10, 33, 704, DateTimeKind.Local).AddTicks(350), false, "Transfer", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AccountTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AccountTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionStatuses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionStatuses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionStatuses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionStatuses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TransactionTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
